b4bbee495aaa340c5b9e0ce7df21fe892601a658
cyber.root@gmail.com <cyber.root@gmail.com@7efa4bdb-9525-3cff-05fb-cf9e01b12c13>
2012-08-22 04:47:01 +0000

    la cache de smarty todavia presente

--- trunk/phpost/cache/%%11^11C^11CE82B6%%m.global_ads_160.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2012-08-16 19:32:48
-         compiled from modules/m.global_ads_160.tpl */ ?>
-					<center>
-                    	<?php echo $this->_tpl_vars['tsConfig']['ads_160']; ?>
-
-                    </center>
