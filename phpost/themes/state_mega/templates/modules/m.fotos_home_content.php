5d5632e20f0f7ed885284433cb829f4dda34f3cc
cyber.root <cyber.root@gmail.com>
2012-09-10 21:54:15 +0000

    monitor y mensajes

--- phpost/themes/state_mega/templates/modules/m.fotos_home_content.php
+++ phpost/themes/state_mega/templates/modules/m.fotos_home_content.php
@@ -1,28 +1,26 @@
                 <div id="centroDerecha" style="width: 630px; float: left;">
                 	
-					<div class="">
-                        
-						<h2 style="font-size: 15px;">&Uacute;ltimas fotos</h2>
-                    
-					</div>
-                    
+					<div class="">                        
+						<h2 style="font-size: 15px;">&Uacute;ltimas fotos</h2>                    
+					</div>                    
 					<ul class="fotos-detail listado-content">
-                        
-						{foreach from=$tsLastFotos.data item=f}
-                    	
+                                                <?
+						foreach ($tsLastFotos['data'] as $f)
+                                                {
+                                                ?>
 						<li>
                         	
 							<div class="avatar-box" style="z-index: 99;">
                             	
-								<a href="<? echo $tsConfig['url']; ?>/fotos/{$f.user_name}/{$f.foto_id}/{$f.f_title|seo}.html">                            		
-                                                                    <img height="100" width="100" <? if ($f['f_status'] != 0 || $f['user_activo'] == 0 || $f['user_baneado'] == 1) { echo 'class="qtip"';?> title="<? if ($f['f_status'] == 2) echo 'Imagen eliminada'; elseif ($f.f_status == 1) echo 'Imagen oculta por acumulaci&oacute;n de denuncias'; elseif ($f.user_activo == 0) echo 'La cuenta del usuario est&aacute; desactivada'; elseif ($f['user_baneado'] == 1) echo 'La cuenta del usuario est&aacute; suspendida'; ?>" style="border: 1px solid <? if ($f['f_status'] == 2) echo 'rosyBrown'; elseif( $f['f_status'] == 1) echo 'coral'; elseif ($f['user_activo'] == 0) echo 'brown'; elseif ($f['user_baneado'] == 1) echo 'orange'; ?>;opacity: 0.5;filter: alpha(opacity=50);" <? } ?> src="<? echo $f['f_url'];?>" />                                
+                                                            <a href="<? echo $tsConfig['url']; ?>/fotos/<? echo $f['user_name']; ?>/<? echo $f['foto_id']; ?>/<? echo string_seo($f['f_title']); ?>.html">
+                                                                    <img height="100" width="100" <? if ($f['f_status'] != 0 || $f['user_activo'] == 0 || $f['user_baneado'] == 1) { echo 'class="qtip"';?> title="<? if ($f['f_status'] == 2) echo 'Imagen eliminada'; elseif ($f.f_status == 1) echo 'Imagen oculta por acumulaci&oacute;n de denuncias'; elseif ($f['user_activo'] == 0) echo 'La cuenta del usuario est&aacute; desactivada'; elseif ($f['user_baneado'] == 1) echo 'La cuenta del usuario est&aacute; suspendida'; ?>" style="border: 1px solid <? if ($f['f_status'] == 2) echo 'rosyBrown'; elseif( $f['f_status'] == 1) echo 'coral'; elseif ($f['user_activo'] == 0) echo 'brown'; elseif ($f['user_baneado'] == 1) echo 'orange'; ?>;opacity: 0.5;filter: alpha(opacity=50);" <? } ?> src="<? echo $f['f_url'];?>" />                                
 								</a>
                             
 							</div>                            
 							<div class="notification-info">                            	
 								<span>                                    
-									<a href="<? echo $tsConfig['url']; ?>/fotos/<? echo $f['user_name']; ?>/{$f.foto_id}/{$f.f_title|seo}.html"><? echo $f['f_title']; ?></a><br />                             		
-									<span title="{$f.f_date|date_format:"%d.%m.%y a las %H:%M hs."}" class="time">
+                                                                    <a href="<? echo $tsConfig['url']; ?>/fotos/<? echo $f['user_name']; ?>/<? echo $f['foto_id']; ?>/<? echo string_seo($f['f_title']); ?>.html"><? echo $f['f_title']; ?></a><br />                             		
+                                                                    <span title="<? echo modifier_hace($f['f_date']); ?> |date_format:"%d.%m.%y a las %H:%M hs."}" class="time">
                                                                               <strong><? echo $f['f_date'];?>date_format:"%d.%m.%Y"}</strong> - Por 
                                                                             <a href="<? echo $tsConfig['url']; ?>/perfil/<? echo $f['user_name']; ?>"><? echo $f['user_name']; ?></a></span><hr />                                    
                                                                         <span class="time"><? echo string_truncate($f['f_description'],94); ?></span>
@@ -32,11 +30,10 @@
 							</div>
                         
 						</li>
-                        
-						{/foreach}
+                                                <?
+                                                }
+                                                ?>
 						
-                    </ul>
-					
-				{if $tsLastFotos.data > 10}P&aacute;ginas: {$tsLastFotos.pages}{/if}
-					
+                    </ul>					
+				<? if ($tsLastFotos['data'] > 10) echo 'P&aacute;ginas: '.$tsLastFotos['pages']; ?>
                 </div>
---
+++ phpost/themes/state_mega/templates/t.php_files/p.mensajes.lista.php
@@ -0,0 +1,30 @@
+<?
+if ($tsMensajes['data'])
+{
+    foreach ($tsMensajes['data'] as $mp)
+    {
+    ?>
+	<li<? if ($mp['mp_read_to'] == 0 || $mp['mp_read_mon_to'] == 0) echo ' class="unread"';   ?>>
+        <a href="<? echo $tsConfig['url']; ?>/mensajes/leer/<? echo $mp['mp_id']; ?>" title="<? echo $mp['mp_subject']; ?>">
+            <img src="<? echo $tsConfig['url']; ?>/files/avatar/<? echo $mp['mp_from']; ?>_50.jpg" alt="<? echo $mp['user_name']; ?>"/>
+            <div class="content clearfix">
+                <div class="subject"><? echo $mp['mp_subject']; ?></div>
+                <div class="preview">
+                    <? echo $mp['mp_preview']; ?>
+                </div>
+                <div class="time">
+                    <span class="autor"><? echo $mp['user_name']; ?></span> | <? echo modifier_hace($mp['mp_date']); ?>|fecha}
+                </div>
+            </div>
+        </a>
+    </li>
+    <?
+    }
+}
+else
+{
+?>    
+    <div class="emptyData">No tienes mensajes</div>
+    <?
+}
+    ?>
