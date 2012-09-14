8e1d0c3aea9d058edc8f70224798f4e155a0a8b9
cyber.root@gmail.com <cyber.root@gmail.com@7efa4bdb-9525-3cff-05fb-cf9e01b12c13>
2012-09-14 01:05:00 +0000

    casi listo la esta cagada jejeje

--- trunk/phpost/themes/state_mega/templates/sections/head_submenu.php
+++ trunk/phpost/themes/state_mega/templates/sections/head_submenu.php
@@ -1,4 +1,4 @@
-		<div class="subMenuContent">
+	<div class="subMenuContent">
         	<div id="subMenuPosts" class="subMenu <? if($tsPage != 'tops') echo 'here'; ?>">
                 <ul class="floatL tabsMenu">
                     <li<? if ($tsPage == 'home' || $tsPage == 'portal') echo ' class="here"'; ?>>
@@ -6,9 +6,10 @@
                     </li>
                     <li<? if ($tsPage == 'buscador') echo ' class="here"'; ?>>
                         <a class=vctip title="Buscador" href="<? echo $tsConfig['url']; ?>/buscador/">Buscador</a></li>
-                    <? if ($tsUser->is_member) 
-                        {                     
-                        if ($tsUser->is_admod || $tsUser->permisos['gopp'])
+                    <? 
+                    if ($tsUser->is_member) 
+                    {                     
+                       if ($tsUser->is_admod || $tsUser->permisos['gopp'])
                         {
                     ?>
                     <li<? if ($tsSubmenu == 'agregar') echo ' class="here"'; ?>>
@@ -17,15 +18,23 @@
                     <li class="<? if($tsPage == 'mod-history') echo 'here' ?>">
                         <a class=vctip title="Historial de Moderaci&oacute;n" href="<? echo $tsConfig['url']; ?>/mod-history/">Historial</a>
                     </li>
-        	            <? if ($tsUser->is_admod || $tsUser->permisos['moacp']) ?>
+        	    <?
+                    if ($tsUser->is_admod || $tsUser->permisos['moacp'])
+                    {   
+                    ?>
                     <li class="<? if($tsPage == 'moderacion') echo 'here'; ?>">
                         <a class=vctip title="Panel de Moderador" href="<? echo $tsConfig['url']; ?>/moderacion/">Moderaci&oacute;n 
-                            <? if ($tsConfig['c_see_mod'] && $tsConfig['novemods']['total']){ ?>
+                            <? if ($tsConfig['c_see_mod'] && $tsConfig['novemods']['total'])
+                            { 
+                            ?>
                             <span class="cadGe cadGe_<? if ($tsConfig['novemods']['total'] < 10) echo 'green'; elseif ($tsConfig['novemods']['total'] < 30) echo 'purple'; else echo 'red'; ?>" style="position:relative;"><? echo $tsConfig['novemods']['total'];?> </span>
-                            <? } ?>
+                            <?                            
+                            } 
+                            ?>
                         </a>
                     </li>
                     <?                    
+                     }
                     }                    
                     }
                     ?>
@@ -42,17 +51,19 @@
                     <? if ($tsAction == 'album' && $tsFUser[0] != $tsUser->uid)
                     {
                     ?>    
-                    <li class="here"><a href="<? echo $tsConfig['url']; ?>/fotos/<? echo $tsFUser['1']; ?>">&Aacute;lbum de <? echo $tsFUser['1']; ?></a></li>
+                    <li class="here">
+                        <a href="<? echo $tsConfig['url']; ?>/fotos/<? echo $tsFUser[1]; ?>">&Aacute;lbum de <? echo $tsFUser[1]; ?></a></li>
                     <?                     
                     }
-                    if ($tsUser->is_admod || $tsUser->permisos.gopf) 
+                    if ($tsUser->is_admod || $tsUser->permisos['gopf']) 
                     {
                     ?>
-                    <li<? if( $tsAction == 'agregar') echo 'class="here"'; ?>><a href="<? echo $tsConfig['url']; ?>/fotos/agregar.php">Agregar Foto</a></li>
+                    <li<? if( $tsAction == 'agregar') echo ' class="here"'; ?>>
+                        <a href="<? echo $tsConfig['url']; ?>/fotos/agregar.php">Agregar Foto</a></li>
                     <?
                     }
                     ?>
-                    <li<? if ($tsAction == 'album' && $tsFUser[0] == $tsUser->uid) echo 'class="here"'; ?> >
+                    <li <? if ($tsAction == 'album' && $tsFUser[0] == $tsUser->uid) echo 'class="here"'; ?> >
                         <a href="<? echo $tsConfig['url'].'/fotos/'.$tsUser->nick; ?>">Mis Fotos</a>
                     </li>
                 </ul>
@@ -60,8 +71,8 @@
             </div>
             <div id="subMenuTops" class="subMenu <? if($tsPage == 'tops') echo 'here'; ?>">
                 <ul class="floatL tabsMenu">
-                    <li<? if ($tsAction == 'posts') echo 'class="here"'; ?>><a href="<? echo $tsConfig['url']; ?>/top/posts/">Posts</a></li>
-                    <li<? if ($tsAction == 'usuarios') echo 'class="here"'; ?>><a href="<? echo $tsConfig['url']; ?>/top/usuarios/">Usuarios</a></li>
+                    <li <? if ($tsAction == 'posts') echo 'class="here"'; ?>><a href="<? echo $tsConfig['url']; ?>/top/posts/">Posts</a></li>
+                    <li <? if ($tsAction == 'usuarios') echo 'class="here"'; ?>><a href="<? echo $tsConfig['url']; ?>/top/usuarios/">Usuarios</a></li>
                 </ul>
                 <div class="clearBoth"></div>
             </div>
